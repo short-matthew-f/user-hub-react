a6bf243c2bbd47fd54589cb8dbabb290b977bc74
Matthew Short <short.matthew.f@gmail.com>
2020-05-20 20:52:20 -0400

    hash-router

---
+++ build/precache-manifest.7cdfb788c36d219724f246c92e08fdd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0effe2624e711a889d6c936665a2db09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "364959fa5caf89b7cfa0",
+    "url": "/static/css/main.1253bac1.chunk.css"
+  },
+  {
+    "revision": "6ce42b93a2f94650052b",
+    "url": "/static/js/2.87b34935.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.87b34935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364959fa5caf89b7cfa0",
+    "url": "/static/js/main.c2f1ce90.chunk.js"
+  },
+  {
+    "revision": "d46d90a666a8fdbe959e",
+    "url": "/static/js/runtime-main.b70e8828.js"
+  }
+]);
